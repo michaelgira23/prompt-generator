15f3d39c62856533049d64f3112680762b8307a9
Michael Gira <contact@michaelgira.me>
2018-03-03 01:51:08 -0600

    Add form/logic for selecting a random prompt

--- src/generator.js
+++ src/generator.js
@@ -4,8 +4,40 @@ let themes = [];
 let mediums = [];
 let tools = [];
 
+const $body = document.querySelector('body');
+
+const $form = document.querySelector('.form');
+const $amountInput = document.querySelector('#amount');
+const $detailInput = document.querySelector('#detail');
+
+const $promptList = document.querySelector('.prompts');
+
 // Start
 gapi.load('client', start);
+
+// When "Generate" is clicked
+function generate() {
+	console.log('Generate', $amountInput.value, $detailInput.value)
+	const amount = Number($amountInput.value);
+
+	clearPrompts();
+
+	for (let i = 0; i < amount; i++) {
+		let theme = null;
+		let medium = null;
+		let tool = null;
+
+		switch ($detailInput.value) {
+			case 'elaborate':
+				medium = randomItem(mediums);
+				tool = randomItem(tools);
+			case 'simple':
+				theme = randomItem(themes);
+		}
+
+		addPrompt({ theme, medium, tool });
+	}
+}
 
 // On Google API load
 function start() {
@@ -46,4 +78,51 @@ function getData() {
 		range: `D2:F`,
 		majorDimension: 'COLUMNS'
 	});
+}
+
+// Add a prompt to the results
+function addPrompt({ theme, medium, tool }) {
+	console.log('Add prompt', theme, medium, tool);
+	$body.classList.add('populated');
+
+	const $prompt = document.createElement('div');
+	$prompt.classList.add('prompt');
+	if (theme) {
+		$prompt.append(createDataElement('Theme', theme));
+	}
+	if (medium) {
+		$prompt.append(createDataElement('Medium', medium));
+	}
+	if (tool) {
+		$prompt.append(createDataElement('Tool', tool));
+	}
+	$promptList.append($prompt);
+}
+
+function createDataElement(label, value) {
+	const $el = document.createElement('div');
+	$el.classList.add(label);
+
+	const $strong = document.createElement('strong');
+	$strong.textContent = `${label}: `;
+	$el.append($strong);
+
+	const $span = document.createElement('span');
+	$span.textContent = value;
+	$el.append($span);
+
+	return $el;
+}
+
+// Clear prompts
+function clearPrompts() {
+	while ($promptList.firstChild) {
+		$promptList.removeChild($promptList.firstChild);
+	}
+	$body.classList.remove('populated');
+}
+
+// Pick random element from array
+function randomItem(array) {
+	return array[Math.floor(Math.random() * array.length)];
 }
